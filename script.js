09db69aa0d96a6320aebc69512ed0450525a7599
jddeltoro <jdiazdeltoro@gmail.com>
2024-02-10 17:23:24 -0500

    add  logic to capture input value

---
+++ script.js
@@ -0,0 +1,24 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const decimalToBinary = (input) => {
+
+};
+
+const checkUserInput = () => {
+    if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+        alert("Please provide a decimal number");
+        return;
+    }
+
+    console.log(numberInput.value);
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        checkUserInput();
+    }
+});
